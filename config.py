a0494cbde5f27a0a51f67e9a42eecf8d3111b8ed
alecksandr <sansepiol26@gmail.com>
2023-04-12 11:19:58 -0600

    uploading

--- config.py
+++ config.py
@@ -10,5 +10,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     # Fetch some of the veriables from the environment
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+
+    # Creates a database on the project dir
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir,
+                                                                                            'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
